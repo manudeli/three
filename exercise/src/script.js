981d0fcba698b506057a90c84bc464a9f477d924
Jonghyeon Ko <manudeli.ko@gmail.com>
2022-08-07 20:17:56 +0900

    add group, mesh, axeshelper, reorder, rotation, position

--- exercise/src/script.js
+++ exercise/src/script.js
@@ -1,6 +1,9 @@
 import './style.css'
 import * as THREE from 'three'
 
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
 // Scene
 const scene = new THREE.Scene()
 
@@ -9,6 +12,37 @@ const geometry = new THREE.BoxGeometry(1, 1, 1)
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
+
+const group = new THREE.Group()
+scene.add(group)
+
+const cube1 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+)
+
+const cube2 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0xbb0000 })
+)
+
+cube2.position.x = 2
+
+group.add(cube1, cube2)
+
+// Axes Helper
+const axesHelper = new THREE.AxesHelper(2)
+scene.add(axesHelper)
+
+// Scale
+mesh.scale.set(2, 0.5, 0.5)
+
+// Rotation
+mesh.rotation.reorder('YXZ')
+mesh.rotation.x = Math.PI * 0.25
+mesh.rotation.y = Math.PI * 0.25
+
+mesh.position.y = 2
 
 // Sizes
 const sizes = {
@@ -19,11 +53,12 @@ const sizes = {
 // Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
+camera.position.y = 1
+camera.position.x = 1
 scene.add(camera)
+camera.lookAt(mesh.position)
 
 // Renderer
-const renderer = new THREE.WebGLRenderer({
-  canvas: document.querySelector('canvas.webgl'),
-})
+const renderer = new THREE.WebGLRenderer({ canvas })
 renderer.setSize(sizes.width, sizes.height)
 renderer.render(scene, camera)
